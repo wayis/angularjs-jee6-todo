5838c7614b152016bb40cb257f460996fc25f13a
Luc LABBE <luc.labbe@inge.neo-soft.fr>
2014-01-22 17:22:21 +0100

    Use the MongoDB ApplicationConfig to serialize correctly Mongo Object.

--- src/main/java/fr/neosoft/poc/todo/server/application/ApplicationConfig.java
+++ src/main/java/fr/neosoft/poc/todo/server/application/ApplicationConfig.java
@@ -1,16 +1,16 @@
 package fr.neosoft.poc.todo.server.application;
 
+import com.mongodb.jee.jaxrs.JaxrsMongoApplication;
 import fr.neosoft.poc.todo.server.rest.resource.TodoResource;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
 import java.util.Set;
 
 /**
  * Created by l.labbe on 22/01/14.
  */
 @ApplicationPath("/api/v1")
-public class ApplicationConfig extends Application {
+public class ApplicationConfig extends JaxrsMongoApplication {
 
     @Override
     public Set<Class<?>> getClasses() {
